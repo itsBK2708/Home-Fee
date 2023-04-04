d222719e4cc773f2f61db150e46e2a8345a26495
Bhuvanesh <114173705+itsBK2708@users.noreply.github.com>
2023-04-04 14:32:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] jobs = { 100, 200, 300, 400 };
+            Console.WriteLine("Select the jobs:\n1.Window\n2.Floor\n3.Bathroom\n4.Full House");
+            int j=int.Parse(Console.ReadLine());
+            int jb=jobs[j];
+            Console.WriteLine("Enter the number of hours:");
+            int h=int.Parse(Console.ReadLine());
+            Console.WriteLine("The Final amount will be:{0}", jb*h );
+            Console.ReadKey();
+
+        }
+    }
+}
